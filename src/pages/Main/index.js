d78ffb487682dacc79fa0cae591daae07c5f68e5
Rogério Grando <rgrando.unique@gmail.com>
2019-09-25 12:12:42 -0300

    Finalizndo projeto

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
-import { Container, Form, SubmitButton, List } from './styles';
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
+import { Link } from 'react-router-dom';
 
 export default class Main extends Component {
   state = {
@@ -71,7 +73,9 @@ export default class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
